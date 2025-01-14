ac23bf72f60c8b0dc842d3ef283127b875c4f542
sbin0819 <sbinha123@gmail.com>
2025-01-14 13:19:11 +0900

    feat: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:bamtol_market_app/firebase_options.dart';
 import 'package:bamtol_market_app/src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
